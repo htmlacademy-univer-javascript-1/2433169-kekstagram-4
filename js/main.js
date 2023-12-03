b89a3823059214269540e52beb30d3de51fbc6a3
Anya <annamatuhina399@gmail.com>
2023-12-03 14:48:26 +0500

    8,14

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 import './data.js';
 import {PHOTOS_COUNT, addmainObject } from './data.js';
 import {createPictures} from './photoModule.js';
+import { initPictures } from './big-picture.js';
 
 createPictures(Array.from( {length: PHOTOS_COUNT}, addmainObject));
+const pictures = Array.from( {length: PHOTOS_COUNT}, addmainObject);
+
+createPictures(pictures);
+
+initPictures(pictures);
